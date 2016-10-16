f521ce4526bb1749201f5b90164b700ac330564e
Samir Novruzov <samir89.96@mail.ru>
2016-10-16 14:02:14 +0300

    Update SolEq.cpp

--- SolEq.cpp
+++ SolEq.cpp
@@ -1,6 +1,3 @@
-//Этот вариант кода промежуточный, полностью готовый код я показывал тебе на семинаре)
-//Ты мне даже зачел ее, и мы перешли на Евгения Онегина. К сожалению,он у меня не сохранился.вот все,что от него осталось
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -14,11 +11,12 @@
 const int    INF_Roots = -1 ;
 const double EPS       = 0.000001;
 
-int IsZero ( double a );
-int SolEq  ( double a, double b, double c, double *x1, double *x2 );
+int IsZero   ( double a );
+int SolEq    ( double a, double b, double c, double *x1, double *x2 );
 int LinearEq ( double b, double c, double *x1, int nRoots );
-int SqEq ( double a, double b, double c, double *x1, double *x2, int nRoots );
-
+int SqEq     ( double a, double b, double c, double *x1, double *x2, int nRoots );
+void control ( int nRoots, int x1, int x2 );
+	
 int main ()
 {
     setlocale ( LC_ALL, " Rus " );
@@ -31,43 +29,46 @@ int main ()
     printf ( " \n " );
 
     int nRoots = SolEq ( a, b, c, &x1, &x2 );
-
-    // Как я уже говорил, вынеси switch в отдельную функцию.
-    switch ( nRoots ) 
-		{
-
-            case  2: printf ( " x1 = %lg\n " ,x1 );
-                     printf ( " x2 = %lg\n " ,x2 );
-                     break;
-            case  1: printf ( " x = %lg\n "  ,x1 );
-                     break;
-            case  0: printf ( " Íåò ðåøåíèÿ " );
-                     break;
-            case -1: printf ( " Ìíîæåñòâî ðåøåíèé " );
-                     break;
-
-            default: printf ( " nRoots íå ðàâåí íè îäíîìó âàðèàíòó. Ñîâåò: Ïðîâåðèòü ïåðåìåííûå " );
-                     break;
-        }
+    control ( nRoots, x1, x2 );
     return 0;
 }
 
 int IsZero ( double  a )
 {
-    return ( fabs(a) < EPS );
+    return ( fabs ( a ) < EPS );
 }
 
-//assert
-int SolEq (double a, double b, double c, double *x1, double *x2)
+void control ( int nRoots, int x1, int x2 )
 {
-    if ((x1 != x2 ) && ( x1 != NULL && x2 != NULL ))
+    switch ( nRoots ) 
+	{	
+	case  2: printf ( " x1 = %lg\n " ,x1 );
+                 printf ( " x2 = %lg\n " ,x2 );
+                 break;
+        case  1: printf ( " x = %lg\n "  ,x1 );
+                 break;
+        case  0: printf ( " Íåò ðåøåíèÿ " );
+                 break;
+        case -1: printf ( " Ìíîæåñòâî ðåøåíèé " );
+                 break;
+
+        default: printf ( " nRoots íå ðàâåí íè îäíîìó âàðèàíòó. Ñîâåò: Ïðîâåðèòü ïåðåìåííûå " );
+                 break;
+       	 }
+}
+
+int SolEq ( double a, double b, double c, double *x1, double *x2 )
+{
+    assert ( x1 );
+    assert ( x2 );
+    if ( x1 != x2 )
     {
         int nRoots = 0;
 
-        if ( IsZero (a) && IsZero (b) )
+        if ( IsZero ( a ) && IsZero ( b ) )
             return  ( INF_Roots );
 
-        else if ( IsZero (a) )
+        else if ( IsZero ( a ) )
             nRoots = LinearEq ( b, c, x1, nRoots );
 
         else if(  !IsZero (a) )
@@ -78,7 +79,7 @@ int SolEq (double a, double b, double c, double *x1, double *x2)
     return ( 404 );
 }
 
-int LinearEq (double b, double c, double *x1, int nRoots)
+int LinearEq ( double b, double c, double *x1, int nRoots )
 {
     //bx+c=0
     *x1 = -c / b;
@@ -86,26 +87,26 @@ int LinearEq (double b, double c, double *x1, int nRoots)
     return nRoots;
 }
 
-int  SqEq (double a, double b, double c, double *x1, double *x2, int nRoots)
+int  SqEq ( double a, double b, double c, double *x1, double *x2, int nRoots )
 {
-        //ax^2+bx+c=0
-    double d = b*b - 4*a*c;
+    //ax^2+bx+c=0
+    double d = b * b - 4 * a * c;
     if ( d < 0 )
     {
-        nRoots=0;
+        nRoots = 0;
     }
     if ( IsZero ( d ) )
     {
 
-        *x1 = -b / ( 2*a );
+        *x1 = -b / ( 2 * a );
         nRoots = 1;
     }
     if ( d > 0 )
     {
 
         d = sqrt( d );
-        *x1 = ( -b + d ) / ( 2*a );
-        *x2 = ( -b - d ) / ( 2*a );
+        *x1 = ( -b + d ) / ( 2 * a );
+        *x2 = ( -b - d ) / ( 2 * a );
         nRoots = 2;
     }
     return nRoots;
