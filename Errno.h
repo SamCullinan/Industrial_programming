c3e19d1bc5176ad7f74600201ce88af09297d206
Samir Novruzov <samir89.96@mail.ru>
2016-10-16 13:35:09 +0300

    Stack

---
+++ Errno.h
@@ -0,0 +1,12 @@
+#ifndef Errno__H
+#define Errno__H
+
+#define NOERROR 0       /* Нет ошибок         */
+#define ERRMAXSIZE 1   /* Переполнение       */
+#define ERRCOUNT 2    /* Проблемы с  count  */
+#define ERRNULL 3    /* Обращение к NULL   */
+#define ERREMPTY 4  /* Пустой массив      */
+#define ERRVAL 5   /* Неверное значение  */
+#define ERRPTR 6  /* Неверный указатель */
+
+#endif Errno__H
