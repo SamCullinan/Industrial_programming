75d8f294c7296e9aa870a78d36b1baa7dbb0b223
Samir Novruzov <samir89.96@mail.ru>
2016-10-16 13:51:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,20 @@
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+	setlocale ( LC_ALL, "Russian" );
 
 	Stack_t Stack;
 
-	Stack_construct(&Stack, 5);
+	Stack_construct ( &Stack, 5 );
 
-	for (int i = 0; i < 5; i++)
-		Stack_push(&Stack, i);
+	for ( int i = 0; i < 5; i++ )
+		Stack_push( &Stack, i );
 
-	for (int i = 0; i < 8; i++)
-		printf("%lg\n", Stack_pop(&Stack));
+	for ( int i = 0; i < 8; i++ )
+		printf ( "%lg\n", Stack_pop( &Stack ) );
 
-	Stack_destruct(&Stack);
+	Stack_destruct ( &Stack );
 
-	system("pause");
+	system ( "pause" );
 	return 0;
 }
