03ae3e1875eecd58323cf9d095497d447f3603d9
Samir Novruzov <samir89.96@mail.ru>
2016-10-18 08:08:55 +0300

    Stack

--- Stack/M_Stack.cpp
+++ Stack/M_Stack.cpp
@@ -6,158 +6,158 @@
 #include <cstring>
 #include "strerrno.h"
 
-int Stack_construct(Stack_t * This, int size)
+int Stack_construct( Stack_t * This, int size )
 {
-	assert(This);
+	assert( This );
 
-	if (This->allocator == 0)
+	if ( This -> allocator == 0 )
 	{
-		This->protection1 = 255;
+		This -> protection1 = 255;
 
-		This->data = (type*)calloc(size, sizeof(*This->data));
-		for (int i = 0; i < size; i++)
-			This->data[i] = Daterror;
-		This->max = size;
-		This->count = 0;
+		This -> data = ( type* )calloc( size, sizeof( *This -> data ) );
+		for ( int i = 0; i < size; i++ )
+			This -> data[ i ] = Daterror;
+		This -> max = size;
+		This -> count = 0;
 
-		This->protection2 = 255;
-		This->allocator = 1;
+		This -> protection2 = 255;
+		This -> allocator = 1;
 
-		This->hash = This->protection1 + This->protection2
-			+ This->max + This->allocator;
+		This -> hash = This -> protection1 + This -> protection2
+			+ This -> max + This -> allocator;
 
-		This->prot_Date = This->data;
+		This -> prot_Date = This -> data;
 	}
 	else
 	{
-		printf("\nТы получил указатель на уже созданный стек!!\n");
+		printf( "\nР’Р°С€ СЃС‚РµРє СѓР¶Рµ СЃРѕР·РґР°РЅ!!\n" );
 
-		ASSERT_OK(This);
+		ASSERT_OK( This );
 
 		return 1;
 	}
 
-	assert(This);
+	assert( This );
 
 	return 0;
 }
 
-int  Stack_destruct(Stack_t * This)
+int  Stack_destruct( Stack_t * This )
 {
-	assert(This);
+	assert( This );
 
-	if (Stack_OK(This))
+	if ( Stack_OK( This ) ) 
 	{
-		This->max = 0;
-		This->count = Error_pos;
-		This->hash = 0;
-		if (This->data != NULL)
+		This -> max = 0;
+		This -> count = Error_pos;
+		This -> hash = 0;
+		if (This -> data != NULL)
 		{
-			free((This->data));
-			This->data = NULL;
-			This->prot_Date = NULL;
+			free( ( This -> data ) );
+			This -> data = NULL;
+			This -> prot_Date = NULL;
 			return 0;
 		}
 		else
 		{
-			ASSERT_OK(This);
+			ASSERT_OK( This );
 			return 1;
 		}
 	}
 	else {
-		printf("Этот стек удален.Возможно он не существовал!!!");
-		ASSERT_OK(This);
+		printf( "РќРµРІРµСЂРЅС‹Р№ СЃС‚РµРє!!!" );
+		ASSERT_OK( This );
 		return 1;
 	}
 }
 
-int  Stack_push(Stack_t * This, int value)
+int  Stack_push( Stack_t * This, int value )
 {
-	assert(Stack_OK(This));
+	assert( Stack_OK( This ) );
 
-	if (This->count >= This->max)
+	if ( This -> count >= This -> max )
 	{
-		printf("Стек переполнен!!!\n");
-		printf("Нельзя добавить элемент!!!\n");
-		abort();
+		printf( "РЎС‚РµРє РїРµСЂРµРїРѕР»РЅРµРЅ!!!\n" );
+		printf( "Р’СЃРµ РїР»РѕС…Рѕ=(!!!\n" );
+		abort( );
 	}
 
-	This->data[This->count++] = value;
+	This -> data[ This -> count++ ] = value;
 
-	assert(Stack_OK(This));
+	assert( Stack_OK(This ) );
 
 	return 0;
 }
 
-type Stack_pop(Stack_t * This)
+type Stack_pop( Stack_t * This )
 {
-	assert(Stack_OK(This));
+	assert( Stack_OK( This ) );
 
-	if (!This->count)
+	if ( !This -> count )
 	{
-		printf("Произошла ошибка при обращении к элементу\n");
-		ASSERT_OK(This);
+		printf( "РћР±Р°С‰РµРЅРёРµ Рє РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РµРјСѓ СЌР»РµРјРµРЅС‚Сѓ\n" );
+		ASSERT_OK( This );
 	}
 
-	This->count--;
-	type top = This->data[This->count];
-	This->data[This->count] = Daterror;
+	This -> count--;
+	type top = This -> data[ This -> count ];
+	This -> data[ This -> count ] = Daterror;
 
-	ASSERT_OK(This);
+	ASSERT_OK( This );
 
 	return top;
 }
 
-bool Stack_OK(const Stack_t * This)
+bool Stack_OK( const Stack_t * This )
 {
-	return  This               &&
-		This->count >= 0 &&
-		This->allocator        &&
-		This->data             &&
-		This->max              &&
-		Stack_Rehash(This) &&
-		This->count <= This->max;
+	return  This                       &&
+		This -> count >= 0         &&
+		This -> allocator          &&
+		This -> data               &&
+		This -> max                &&
+		Stack_Rehash( This )       &&
+		This -> count <= This -> max;
 }
 
-void Stack_dump(Stack_t *This, char* stk)
+void Stack_dump( Stack_t *This, char* stk )
 {
-	printf("Stack_dump\n");
-	printf("Version %d.%d.%d (%s %s)\n\n", 0, 1, 2, __DATE__, __TIME__);
-	printf("=================================\n");
+	printf( "Stack_dump\n" );
+	printf( "Version %d.%d.%d (%s %s)\n\n", 0, 1, 2, __DATE__, __TIME__ );
+	printf( "=================================\n" );
 
-	if (Stack_OK(This))
-		printf("Stack_t \"%s\" (Ok) [%p]\n", stk + 1, This);
+	if ( Stack_OK( This ) )
+		printf( "Stack_t \"%s\" (Ok) [%p]\n", stk + 1, This );
 	else
-		printf("Stack_t \"[%s]\" (Error) [%p]\n", stk, This);
+		printf( "Stack_t \"[%s]\" (Error) [%p]\n", stk, This );
 
-	printf("data[%d] = [%p]\n", This->max, This->data);
-	printf(" {\n");
+	printf( " data[%d] = [%p]\n", This -> max, This -> data );
+	printf( " {\n" );
 
-	for (int i = 0; i < This->max; i++)
+	for ( int i = 0; i < This -> max; i++ )
 	{
-		if (i < This->count)
+		if ( i < This -> count )
 		{
-			if (This->data)
-				printf("   *[%d] = %lg\n", i, This->data[i]);
+			if ( This -> data )
+				printf( "   *[%d] = %lg\n", i, This -> data[ i ] );
 		}
 		else
 		{
-			if (This->data)
-				printf("   [%d] = %lg  Poison!\n", i, This->data[i]);
+			if ( This -> data )
+				printf( "   [%d] = %lg  Poison!\n", i, This -> data[ i ] );
 		}
 	}
 
-	printf(" }\n");
-	printf("count = %d\n", This->count);
-	printf("max   = %d\n", This->max);
-	printf("=================================\n");
+	printf( " }\n" );
+	printf( "count = %d\n", This->count );
+	printf( "max   = %d\n", This -> max );
+	printf( "=================================\n" );
 }
 
-bool Stack_Rehash(const Stack *This)
+bool Stack_Rehash( const Stack *This )
 {
-	type buff = This->protection1 + This->protection2 + This->max
-		+ This->allocator;
-	if (This->hash == buff && This->prot_Date == This->data)
+	type buff = This -> protection1 + This -> protection2 + This -> max
+		+ This -> allocator;
+	if (This -> hash == buff && This -> prot_Date == This -> data )
 		return 1;
 	else
 		return 0;
