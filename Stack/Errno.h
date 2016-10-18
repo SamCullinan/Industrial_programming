7f09cee573a4aaaee22b3adb9358867de2911905
Samir Novruzov <samir89.96@mail.ru>
2016-10-18 08:12:37 +0300

    Тестовый режим

--- Stack/Errno.h
+++ Stack/Errno.h
@@ -1,11 +1,11 @@
 #ifndef  Errno__H
 #define  Errno__H
-ERR_NO(NOERROR, 0, "No errors.")
+ERR_NO(NOERROR,    0, "No errors.")
 ERR_NO(ERRMAXSIZE, 1, "Overflow!\n")
-ERR_NO(ERRCOUNT, 2, "Problems with count!\n")
-ERR_NO(ERRNULL, 3, "Appeal to NULL!\n")
-ERR_NO(ERREMPTY, 4, "Empty array!\n")
-ERR_NO(ERRVAL, 5, "No correct value!\n")
-ERR_NO(ERRPTR, 6, "Invalid pointer!\n")
+ERR_NO(ERRCOUNT,   2, "Problems with count!\n")
+ERR_NO(ERRNULL,    3, "Appeal to NULL!\n")
+ERR_NO(ERREMPTY,   4, "Empty array!\n")
+ERR_NO(ERRVAL,     5, "No correct value!\n")
+ERR_NO(ERRPTR,     6, "Invalid pointer!\n")
 
 #endif Errno__H
